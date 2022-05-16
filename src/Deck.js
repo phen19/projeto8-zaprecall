5e5d35e365d5f1de57e104473b70f32362aa7ee4
Paulo Henrique Lopes <phenriquelgsilva@gmail.com>
2022-05-16 13:49:54 -0300

    bug corrected deck selection

--- src/Deck.js
+++ src/Deck.js
@@ -5,11 +5,11 @@ export default function Deck({setStart, setDeck, deckID}){
         <div className="content">
                     <img src={logo} alt="Logo" />
                     <h1>Zap Recall</h1>
-                    <select>
+                    <select {e => setDeck(e.target.value)}>
                         <option value="" disabled selected hidden > Escolha seu deck</option>
-                        <option value="0" onClick={() => setDeck(0)}>JS</option>
-                        <option value="1" onClick={() => setDeck(1)}>Lolzinho</option>
-                        <option value="2" onClick={() => setDeck(2)}>O Maior de todos</option>
+                        <option value="0">JS</option>
+                        <option value="1">Lolzinho</option>
+                        <option value="2">O Maior de todos</option>
                     </select>
                     <button className="disabled" disabled >Iniciar Recall</button>
         </div>
@@ -19,11 +19,11 @@ export default function Deck({setStart, setDeck, deckID}){
             <div className="content">
                         <img src={logo} alt="Logo" />
                         <h1>Zap Recall</h1>
-                        <select>
+                        <select{e => setDeck(e.target.value)}>
                             <option value="" disabled selected hidden > Escolha seu deck</option>
-                            <option value="0" onClick={() => setDeck(0)}>JS</option>
-                            <option value="1" onClick={() => setDeck(1)}>Lolzinho</option>
-                            <option value="2" onClick={() => setDeck(2)}>O Maior de todos</option>
+                            <option value="0">JS</option>
+                            <option value="1">Lolzinho</option>
+                            <option value="2">O Maior de todos</option>
                         </select>
                 <button onClick={setStart}>Iniciar Recall</button>
             </div>
